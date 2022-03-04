41ed164363ae2b500009d65f062a5aa92ae2c1ab
Ivan Zrvnar <izrvnar@gmail,com>
2022-03-04 11:32:23 -0500

    added more labels and other items in each view as a skeleton idea of what the app will look like. Began working on the data source and custom cell

--- IvanZrvnarTest1/Controller/ViewController.swift
+++ IvanZrvnarTest1/Controller/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: -Data Source
+    private lazy var dataSource = AlbumDataSource(tableView: tableView){
+        tableView, indexPath, album in
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AlbumCell", for: indexPath)
+        
+        
+        
+        return cell
+    }
+    
+    
+    
+    
+    //MARK: - Properties
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
 
+    
+    //MARK: -VIEW DID LOAD
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+    }//: View did load
 
 
-}
+}//: View controller
 
